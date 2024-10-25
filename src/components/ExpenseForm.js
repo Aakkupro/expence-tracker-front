66f9164c199bb03fe3d68238ba35968ea8859fbb
Aakanksha Prasad <aakankshaprasad6@gmail.com>
2024-10-25 11:11:14 +0530

    want to commit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-//import axios from 'axios';
 
 const ExpenseForm = ({ fetchExpenses, editingExpense, onSubmit }) => {
     const [formData, setFormData] = useState({ name: '', amount: '', category: '', date: '' });
